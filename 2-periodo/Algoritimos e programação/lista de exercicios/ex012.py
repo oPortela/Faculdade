4b30a89384f3b9cc50ffb22bbfa488d32d19d778
Matheus Marques <matheusmarquesportela@gmail.com>
2023-09-06 16:09:53 -0300

    update

--- 2-periodo/Algoritimos e programação/lista de exercicios/ex012.py
+++ 2-periodo/Algoritimos e programação/lista de exercicios/ex012.py
@@ -7,7 +7,7 @@ if pergunta1.lower() == "sim":
         animal = "águia"
     else:
         animal = "pato"
-else:
+elif pergunta1.lower() == "não":
     pergunta3 = input("O animal é um mamífero? ")
     if pergunta3.lower() == "sim":
         pergunta4 = input("O animal é um carnívoro? ")
@@ -24,34 +24,46 @@ else:
                 if pergunta7.lower() == "sim":
                     animal = "baleia"
                 else:
-                    animal = "ornitorrinco"
-            else:
-                animal = "ornitorrinco"
-    else:
-        pergunta7 = input("O animal vive na água? ")
-        if pergunta7.lower() == "sim":
-            pergunta8 = input("O animal é um peixe? ")
-            if pergunta8.lower() == "sim":
-                pergunta9 = input("O animal é um tubarão? ")
-                if pergunta9.lower() == "sim":
-                    animal = "tubarão"
-                else:
-                    animal = "lambari"
-            else:
-                pergunta10 = input("O animal é uma enguia? ")
-                if pergunta10.lower() == "sim":
-                    animal = "enguia"
-                else:
-                    pergunta11 = input("O animal é uma arraia? ")
-                    if pergunta11.lower() == "sim":
-                        animal = "arraia"
+                    pergunta8 = input("O animal é um ornitorrinco? ")
+                    if pergunta8.lower() == "sim":
+                        animal = "ornitorrinco"
                     else:
-                        animal = "pinguim"
-        else:
-            pergunta12 = input("O animal é um morcego? ")
-            if pergunta12.lower() == "sim":
-                animal = "morcego"
+                        animal = "onça pintada"
             else:
-                animal = "galinha"
+                pergunta9 = input("O animal vive na água? ")
+                if pergunta9.lower() == "sim":
+                    pergunta10 = input("O animal é um peixe? ")
+                    if pergunta10.lower() == "sim":
+                        pergunta11 = input("O animal é um tubarão? ")
+                        if pergunta11.lower() == "sim":
+                            animal = "tubarão"
+                        else:
+                            animal = "lambari"
+                    else:
+                        pergunta12 = input("O animal é uma enguia? ")
+                        if pergunta12.lower() == "sim":
+                            animal = "enguia"
+                        else:
+                            pergunta13 = input("O animal é uma arraia? ")
+                            if pergunta13.lower() == "sim":
+                                animal = "arraia"
+                            else:
+                                animal = "pinguim"
+                else:
+                    pergunta14 = input("O animal é um morcego? ")
+                    if pergunta14.lower() == "sim":
+                        animal = "morcego"
+                    else:
+                        pergunta15 = input("O animal é uma galinha? ")
+                        if pergunta15.lower() == "sim":
+                            animal = "galinha"
+                        else:
+                            pergunta16 = input("O animal é um avestruz? ")
+                            if pergunta16.lower() == "sim":
+                                animal = "avestruz"
+                            else:
+                                animal = "onça pintada"
+else:
+    animal = "animal não identificado"
 
 print(f"O animal que você está pensando é um(a) {animal}.")
